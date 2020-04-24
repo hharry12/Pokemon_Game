b519ee5ea8d5fdda27feceba625007f549935443
Cole O'Brien <gastrocole@gmail.com>
2020-04-24 19:57:06 +0100

    refactored classes, added packages for MCV, added initial game class

---
+++ controller/game.py
@@ -0,0 +1,27 @@
+class Game:
+    if __name__ == "__main__":
+        main()
+    
+    def main():
+        initialiseGame()
+        runGame()
+        endGame()
+
+    def initialiseGame():
+        #initialise game variables
+        print("No code in initialise game method")
+
+    def runGame()
+        #start the game loop
+        while(true){
+            #wait for user input
+            command = input()
+            #echo command for the time being
+            print(command)
+        }
+
+    def endGame()
+        #clean up any data etc when the game ends
+        print("No code in end game method")
+
+
